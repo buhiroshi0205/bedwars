d74395b0f9396e69c981a2740e8122f793d4d560
buhiroshi0205 <buhiroshi@hotmail.com>
2019-12-23 01:22:15 +0900

    idek what I'm doing, someone help

--- src/main/java/bedwars/Main.java
+++ src/main/java/bedwars/Main.java
@@ -1,33 +1,89 @@
 package bedwars;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.Location;
+import org.bukkit.Color;
+import org.bukkit.ChatColor;
+
 import java.util.ArrayList;
 
 public final class Main extends JavaPlugin {
 
-	boolean ingame = false;
+	int gamephase = 0; // 0 = none, 1 = lobby, 2 = ingame
 	FileConfiguration config;
-	ArrayList<Team> teams = new ArrayList<Team>();
+	ArrayList<Team> teams;
 
-	Location loclow;
-	Location lochigh;
+	Location loclow, lochigh;
 
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (gamephase == 1) {
+			switch (label.toLowerCase()) {
+				case "joingame":
+					if (!(sender instanceof Player)) return false;
+					for (Team team : teams) {
+						if (team.name.equals(args[0])) {
+							if (team.players.size() >= team.maxplayers) {
+								sender.sendMessage(ChatColor.RED + "This team is full!");
+							} else {
+								team.addPlayer((Player) sender);
+							}
+							break;
+						}
+					}
+					break;
+				case "startgame":
+					startGame();
+					break;
+			}
+		}
+
+		return true;
+  }
 
 	@Override
 	public void onEnable() {
 		saveDefaultConfig();
-
 		config = getConfig();
+	}
+
+	private void initGame() {
+		gamephase = 1;
+		teams = new ArrayList<Team>();
+
+
+		String warworld = config.getString("region.world");
+		loclow = getLocFromConfig(config, warworld, "region.low");
+		lochigh = getLocFromConfig(config, warworld, "region.high");
+
 		ConfigurationSection allteamconfigs = config.getConfigurationSection("teams");
 		for (String teamname : allteamconfigs.getKeys(false)) {
 			ConfigurationSection teamconfig = allteamconfigs.getConfigurationSection(teamname);
-			
+			// do something with color
+			Location spawn = getLocFromConfig(teamconfig, warworld, "spawn");
+			Location gen = getLocFromConfig(teamconfig, warworld, "generator");
+			Location bed = getLocFromConfig(teamconfig, warworld, "bed");
+			Team newteam = new Team(teamname, Color.AQUA, spawn, gen, bed, config.getInt("maxplayersperteam"));
+			teams.add(newteam);
 		}
+	}
 
+	private void startGame() {
+
+	}
+
+	Location getLocFromConfig(ConfigurationSection configsec, String world, String base) {
+		return new Location(Bukkit.getWorld(world),
+												configsec.getInt(base + ".x"),
+												configsec.getInt(base + ".y"),
+												configsec.getInt(base + ".z"));
 	}
 
 }
--- src/main/java/bedwars/Team.java
+++ src/main/java/bedwars/Team.java
@@ -3,12 +3,30 @@ package bedwars;
 import java.util.ArrayList;
 import org.bukkit.entity.Player;
 import org.bukkit.Location;
+import org.bukkit.Color;
 
 class Team {
 
-  ArrayList<Player> players = new ArrayList<Player>();
-  boolean hasbed = false;
-  int upgrades[] = {0,0,0,0,0,0,0};
+  String name;
+  Color color;
   Location spawn, generator, bed;
+  int maxplayers;
+
+  ArrayList<Player> players = new ArrayList<Player>();
+  boolean hasbed = true;
+  int upgrades[] = {0,0,0,0,0,0,0};
+
+  public Team(String name, Color color, Location spawn, Location gen, Location bed, int maxplayers) {
+    this.name = name;
+    this.color = color;
+    this.spawn = spawn;
+    this.generator = gen;
+    this.bed = bed;
+    this.maxplayers = maxplayers;
+  }
+
+  public void addPlayer(Player p) {
+    players.add(p);
+  }
 
 }
