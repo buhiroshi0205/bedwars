77fe95523589cd9b5b455b1207e2e4fb1d90c053
Dummyc0m <m.steve.jobs@me.com>
2019-12-25 17:48:42 -0500

    merging with upstream

--- src/main/java/bedwars/TeamInfo.java
+++ src/main/java/bedwars/TeamInfo.java
@@ -1,24 +1,12 @@
 package bedwars;
 
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.*;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.Location;
-import org.bukkit.Color;
-import org.bukkit.ChatColor;
-import org.bukkit.Color;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.IllegalStateException;
-
-class TeamInfo {
 public class TeamInfo {
 
   // constant variables
